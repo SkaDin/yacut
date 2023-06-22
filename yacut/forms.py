15386b05f316aa83d4928e9e8b973f5363881a68
Денис Сушков <ga.akam@yandex.ru>
2023-06-22 22:47:07 +0300

    start project

---
+++ yacut/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired, Length, Optional
+
+
+class URLForm(FlaskForm):
+    original_link = URLField(
+        'Ваш вариант длинной ссылки',
+        validators=[DataRequired(message='Обязательное поле'), ]
+    )
+    custom_id = StringField(
+        'Ваш вариант короткой ссылки',
+        validators=[Length(1, 16), Optional()]
+    )
+    submit = SubmitField('Создать')
+
