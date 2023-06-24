2a8a451fa2ac8f25ad229cb3a73dc302b52d65d6
Денис Сушков <ga.akam@yandex.ru>
2023-06-24 22:56:53 +0300

    HARD_DAY_IS_OVER

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,16 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import URL, DataRequired, Length, Optional, Regexp, ValidationError
+
+from yacut.models import URLMap
 
 
 class URLForm(FlaskForm):
     original_link = URLField(
-        'Ваш вариант длинной ссылки',
-        validators=[DataRequired(message='Обязательное поле'), ]
+        'Ваша длинная ссылка',
+        validators=[DataRequired(message='Обязательное поле'), URL(
+            require_tld=True, message='Некорректный URL')]
     )
     custom_id = StringField(
         'Ваш вариант короткой ссылки',
-        validators=[Length(1, 16), Optional()]
+        validators=[Length(1, 16), Optional(), Regexp(
+            r'^[A-Za-z0-9]+$',
+            message='Можно использовать только [A-Za-z0-9] '
+                    'длинной не более 6 символов')]
     )
     submit = SubmitField('Создать')
 
+    def validate_custom_id(self, field):
+        if field.data and URLMap.query.filter_by(short=field.data).first():
+            raise ValidationError(f'Название {field.data} уже занято!')
--- yacut/views.py
+++ yacut/views.py
@@ -1,12 +1,38 @@
-from random import randrange
+import random
+import string
 
-from flask import abort, flash, redirect, render_template, url_for
+from flask import flash, redirect, render_template, url_for
 
 from . import app, db
 from .forms import URLForm
 from .models import URLMap
 
 
-@app.route('/')
-def index_view():
-    return render_template('url.html')
+def generate_seq(length=6):
+    """Функция генерации случайной короткой части длинной 6 символов."""
+    seq = string.ascii_letters + string.digits
+    if not URLMap.query.filter_by(short=seq).first():
+        return ''.join(random.choices(seq, k=length))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def add_url_view():
+    """Функция записи ссылок в БД"""
+    form = URLForm()
+    if form.validate_on_submit():
+        short = form.custom_id.data or generate_seq()
+        url = URLMap(
+            original=form.original_link.data,
+            short=short
+        )
+        db.session.add(url)
+        db.session.commit()
+        flash(url_for('url_view', short=short, _external=True))
+    return render_template('url.html', form=form)
+
+
+@app.route('/<string:short>')
+def url_view(short):
+    """Функция 'склеивания' адреса сайта с составной частью(short)."""
+    return redirect(
+        URLMap.query.filter_by(short=short).first_or_404().original)
